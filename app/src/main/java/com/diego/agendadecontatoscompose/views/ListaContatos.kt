116f2bbece4b2ddc516f1d2452520cd106569cea
Diego Cardoso <diegocar448@hotmail.com>
2025-01-21 10:48:51 -0300

    modifier.statusBarsPadding

--- app/src/main/java/com/diego/agendadecontatoscompose/views/ListaContatos.kt
+++ app/src/main/java/com/diego/agendadecontatoscompose/views/ListaContatos.kt
@@ -1,14 +1,18 @@
 package com.diego.agendadecontatoscompose.views
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
 @Composable
 fun ListaContatos(navController: NavController){
 
-    Column() {
+    Column(
+        modifier = Modifier.statusBarsPadding() // Adiciona padding automaticamente respeitando a barra de status
+    ) {
         Text(text = "Lista de Contatos")
     }
 }
