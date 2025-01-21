d3b9c86ffb66ee727fce62d899b1ce5c7ca12e4d
Diego Cardoso <diegocar448@hotmail.com>
2025-01-21 09:58:36 -0300

    correcao dos conflitos de pacotes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    //id("kotlin-kapt")
+    id("kotlin-kapt")
 }
 
 android {
@@ -50,8 +50,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.support.annotations)
-    implementation(libs.com.google.devtools.ksp.gradle.plugin)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -62,51 +60,6 @@ dependencies {
 
 
 
-    //implementation(libs.androidx.room.runtime)
-
-    // If this project uses any Kotlin source, use Kotlin Symbol Processing (KSP)
-    // See Add the KSP plugin to your project
-    //kapt(libs.androidx.room.compiler)
-
-    // If this project only uses Java source, use the Java annotationProcessor
-    // No additional plugins are necessary
-    //annotationProcessor(libs.androidx.room.compiler)
-
-    // optional - Kotlin Extensions and Coroutines support for Room
-//    implementation(libs.androidx.room.ktx)
-//
-//    // optional - RxJava2 support for Room
-//    implementation(libs.androidx.room.rxjava2)
-//
-//    // optional - RxJava3 support for Room
-//    implementation(libs.androidx.room.rxjava3)
-//
-//    // optional - Guava support for Room, including Optional and ListenableFuture
-//    implementation(libs.androidx.room.guava)
-//
-//    // optional - Test helpers
-//    testImplementation(libs.androidx.room.testing)
-//
-//    // optional - Paging 3 Integration
-//    implementation(libs.androidx.room.paging)
-
     // Jetpack Compose integration
-    implementation(libs.androidx.navigation.compose)
-
-    // Views/Fragments integration
-    implementation(libs.androidx.navigation.fragment)
-    implementation(libs.androidx.navigation.ui)
-
-    // Feature module support for Fragments
-    implementation(libs.androidx.navigation.dynamic.features.fragment)
-
-    // Testing Navigation
-    androidTestImplementation(libs.androidx.navigation.testing)
-
-    // JSON serialization library, works with the Kotlin serialization plugin
-    // implementation(libs.kotlinx.serialization.json)
-
-
-    // To use constraintlayout in compose
-    implementation(libs.androidx.constraintlayout.compose)
+    implementation(libs.androidx.navigation.compose.v284)
 }
