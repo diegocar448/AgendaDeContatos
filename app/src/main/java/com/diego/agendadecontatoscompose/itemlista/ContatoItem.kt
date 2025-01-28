9e27d2d270b83ab25edda81b996943f014ec387b
Diego Cardoso <diegocar448@hotmail.com>
2025-01-28 09:54:18 -0300

    delete

--- app/src/main/java/com/diego/agendadecontatoscompose/itemlista/ContatoItem.kt
+++ app/src/main/java/com/diego/agendadecontatoscompose/itemlista/ContatoItem.kt
@@ -1,32 +1,37 @@
 package com.diego.agendadecontatoscompose.itemlista
 
+import android.app.AlertDialog
 import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
+import com.diego.agendadecontatoscompose.AppDatabase
 import com.diego.agendadecontatoscompose.R
+import com.diego.agendadecontatoscompose.dao.ContatoDao
 import com.diego.agendadecontatoscompose.model.Contato
 import com.diego.agendadecontatoscompose.ui.theme.ShapeCardView
 import com.diego.agendadecontatoscompose.ui.theme.WHITE
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+private lateinit var contatoDao: ContatoDao
 
 @Composable
 fun ContatoItem(
@@ -36,11 +41,40 @@ fun ContatoItem(
     context: Context
 ){
 
+    var scope  = rememberCoroutineScope()
+
     val nome = listaContatos[position].nome
     val sobrenome = listaContatos[position].sobrenome
     val idade = listaContatos[position].idade
     val celular = listaContatos[position].celular
     val uid = listaContatos[position].uid
+
+    val contato = listaContatos[position]
+
+
+    fun alertDialogDeletarContato(){
+        val alertDialog = AlertDialog.Builder(context)
+        alertDialog.setTitle("Deseja Excluir?")
+            .setMessage("Tem certeza?")
+        alertDialog.setPositiveButton("OK"){_,_ ->
+            scope.launch(Dispatchers.IO){
+                contatoDao = AppDatabase.getInstance(context).contatoDao()
+                contatoDao.deletar(uid)
+                //remover contato da lista no front
+                listaContatos.remove(contato)
+            }
+            scope.launch(Dispatchers.Main){
+                navController.navigate("listaContatos")
+                Toast.makeText(context, "Contato removido com sucesso!", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        alertDialog.setNeutralButton("Cancelar"){_,_ ->
+
+        }
+        alertDialog.show()
+    }
 
     Card(
         colors = CardDefaults.cardColors(
@@ -110,7 +144,7 @@ fun ContatoItem(
 
             Button(
                 onClick = {
-
+                    alertDialogDeletarContato()
                 },
                 modifier = Modifier.constrainAs(btDeletar){
                     start.linkTo(btAtualizar.end, margin = 0.dp)
